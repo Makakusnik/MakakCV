a9b06bb7b290c0be8287d9c10154e91738a663a4
Marek Fodor <fodormarek5@gmail.com>
2022-05-05 12:02:03 +0200

    .

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  rewrites: async () => [
+    {
+      source: "/public/myfile.html",
+      destination: "/pages/api/myfile.js",
+    },
+  ],
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
