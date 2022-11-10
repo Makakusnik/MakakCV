2674a61cc944713b4e4d79c76548b237c711d033
Marek Fodor <marek.fodor@aomvsr.com>
2022-11-10 12:54:29 +0100

    Pokus

---
+++ pages/pokus.js
@@ -0,0 +1,15 @@
+import { InlineLink, Text } from "../components/content/content";
+
+export default function Home() {
+  return (
+    <>
+      <Header>WHAT IS THIS</Header>
+      <Main>POKUS</Main>
+      <Footer>
+        <Text>
+          Created by <InlineLink href="/">Marek Fodor</InlineLink>
+        </Text>
+      </Footer>
+    </>
+  );
+}
