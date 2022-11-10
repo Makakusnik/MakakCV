8a0e2dca879d56f068bece5bd5bf7c22f31738e9
MarekFodor <marek.fodor.praca@gmail.com>
2022-11-10 13:16:42 +0100

    fgt

--- pages/pokus.js
+++ pages/pokus.js
@@ -1,5 +1,6 @@
+import styled from "styled-components";
 import { InlineLink, Text } from "../components/content/content";
-``;
+
 export default function Home() {
   return (
     <>
@@ -13,3 +14,34 @@ export default function Home() {
     </>
   );
 }
+
+const Footer = styled.footer`
+  display: flex;
+  color: ${({ theme }) => theme.text};
+  border-top: 3px solid ${({ theme }) => theme.primary};
+  height: 60px;
+  padding: 15px;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin: auto;
+`;
+
+const Header = styled.header`
+  display: flex;
+  background: url("gradientBG.svg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  padding-top: 50px;
+  background-position-y: 100%;
+  min-height: 750px;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
